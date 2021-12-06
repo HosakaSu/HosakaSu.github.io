e1fbbb1a1e09612a05c588993277c8c88b08906b
HosakaSu <liucjseu@outlook.com>
2021-12-06 11:45:37 +0800

    init project

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+initDist(){
+  # echo $1 > base.js
+  npm run build
+  cd docs/.vuepress/dist
+}
+
+initDist "module.exports = '/HosakaSu.github.io/'"
+
+# deploy to github
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:HosakaSu/HosakaSu.github.io.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://HosakaSu:${GITHUB_TOKEN}@github.com/HosakaSu.github.io.git
+  git config --global user.name "HosakaSu"
+  git config --global user.email "liucjseu@outlook.com"
+fi
+
+initGit(){
+  git init
+  git add -A
+  git commit -m "${msg}"
+}
+initGit
+# git push -f $githubUrl main # 推送到github
+git push -f git@github.com:HosakaSu/HosakaSu.github.io.git main:gh-pages
+
+cd -
+rm -rf docs/.vuepress/dist
+
---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,57 @@
+module.exports = [
+  { text: '首页', link: '/' },
+  { text: 'Linux', items: [
+    {text: 'Shell', link: '/pages/7f6637/'},
+    {text: 'Kernel', link: '/pages/310bc5/'},
+  ]},
+  {text: 'Embedded', link: '/', items: [
+      { text: 'Jetson', items: [
+        {text: '系统安装', link: '/pages/c691e1/'},
+        {text: '配置', link: '/pages/00b664/'},
+        {text: 'mcp2518驱动', link: '/pages/c4be2f/'},
+        {text: '交叉编译环境安装', link: '/pages/d6ff42/'},
+        {text: '系统备份与刷机', link: '/pages/e82f05/'},
+      ]},
+  ]},
+  {text: 'Programming', items: [
+    {text: 'C', link: '/pages/6db953/'},
+    {text: 'Python', link: '/pages/f61ebf/'},
+  ]},
+  // { text: '首页', link: '/' },
+  // {
+  //   text: '指南', link: '/pages/a2f161/', items: [
+  //     { text: '主题初衷与诞生', link: '/pages/52d5c3/' },
+  //     { text: '介绍', link: '/pages/a2f161/' },
+  //     { text: '快速上手', link: '/pages/793dcb/' },
+  //     { text: '目录结构', link: '/pages/2f674a/' },
+  //     { text: '核心配置和约定', link: '/pages/33d574/' },
+  //     { text: '自动生成front matter', link: '/pages/088c16/' },
+  //     { text: 'Markdown 容器', link: '/pages/d0d7eb/' },
+  //     { text: 'Markdown 中使用组件', link: '/pages/197691/' },
+  //     {
+  //       text: '相关文章', items: [
+  //         { text: '如何让你的笔记更有表现力', link: '/pages/dd027d/' },
+  //         { text: '批量操作front matter工具', link: '/pages/2b8e22/' },
+  //         { text: '部署', link: '/pages/0fc1d2/' },
+  //         { text: '关于写文章和H1标题', link: '/pages/9ae0bd/' },
+  //         { text: '关于博客搭建与管理', link: '/pages/26997d/' },
+  //         { text: '在线编辑和新增文章的方法', link: '/pages/c5a54d/' },
+  //       ]
+  //     }
+  //   ]
+  // },
+  // {
+  //   text: '配置', link: '/pages/a20ce8/', items: [
+  //     { text: '主题配置', link: '/pages/a20ce8/' },
+  //     { text: '首页配置', link: '/pages/f14bdb/' },
+  //     { text: 'front matter配置', link: '/pages/3216b0/' },
+  //     { text: '目录页配置', link: '/pages/54651a/' },
+  //     { text: '添加摘要', link: '/pages/1cc523/' },
+  //     { text: '修改主题颜色和样式', link: '/pages/f51918/' },
+  //     { text: '评论栏', link: '/pages/ce175c/' },
+  //   ]
+  // },
+  // { text: '插件', link: '/pages/db78e2/' },
+  // { text: '案例', link: '/pages/5d571c/' },
+  // { text: '问答', link: '/pages/9cc27d/' },
+]
